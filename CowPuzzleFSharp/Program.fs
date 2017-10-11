ec95acb4c74c774ec6990a438532205e5c1c13f6
Joel Triemstra <JTriemstra@jtriemstraw7.VML.LAN>
2017-10-10 22:46:59 -0400

    continued experiments

--- CowPuzzleFSharp/Program.fs
+++ CowPuzzleFSharp/Program.fs
@@ -13,26 +13,27 @@ type Tile =
   { Top: int;
     Right: int;
     Bottom: int;
-    Left: int }
+    Left: int;
+    Id: int }
     
-let bag = [|
-        {Top=NOSE_LEFT; Right=SMALL_TAIL; Bottom=LAY_BOTTOM; Left=BIG_TAIL},
-        {Top=LAY_BOTTOM; Right=LAY_BOTTOM; Bottom=BIG_HEAD; Left=SMALL_HEAD},
-        {Top=SMALL_HEAD; Right=BIG_HEAD; Bottom=LAY_BOTTOM; Left=SMALL_HEAD},
-        {Top=BIG_TAIL; Right=NOSE_LEFT; Bottom=LAY_BOTTOM; Left=NOSE_RIGHT},
-        {Top=LAY_TOP; Right=BIG_TAIL; Bottom=NOSE_RIGHT; Left=BIG_TAIL},
-        {Top=LAY_BOTTOM; Right=SMALL_TAIL; Bottom=NOSE_LEFT; Left=BIG_TAIL},
-        {Top=NOSE_RIGHT; Right=BIG_HEAD; Bottom=LAY_TOP; Left=SMALL_HEAD},
-        {Top=BIG_HEAD; Right=SMALL_TAIL; Bottom=NOSE_LEFT; Left=NOSE_RIGHT},
-        {Top=LAY_TOP; Right=BIG_TAIL; Bottom=NOSE_RIGHT; Left=SMALL_HEAD}
-    |]
+let bag = [
+        {Top=NOSE_LEFT; Right=SMALL_TAIL; Bottom=LAY_BOTTOM; Left=BIG_TAIL; Id=0};
+        {Top=LAY_BOTTOM; Right=LAY_BOTTOM; Bottom=BIG_HEAD; Left=SMALL_HEAD; Id=1};
+        {Top=SMALL_HEAD; Right=BIG_HEAD; Bottom=LAY_BOTTOM; Left=SMALL_HEAD; Id=2};
+        {Top=BIG_TAIL; Right=NOSE_LEFT; Bottom=LAY_BOTTOM; Left=NOSE_RIGHT; Id=3};
+        {Top=LAY_TOP; Right=BIG_TAIL; Bottom=NOSE_RIGHT; Left=BIG_TAIL; Id=4};
+        {Top=LAY_BOTTOM; Right=SMALL_TAIL; Bottom=NOSE_LEFT; Left=BIG_TAIL; Id=5};
+        {Top=NOSE_RIGHT; Right=BIG_HEAD; Bottom=LAY_TOP; Left=SMALL_HEAD; Id=6};
+        {Top=BIG_HEAD; Right=SMALL_TAIL; Bottom=NOSE_LEFT; Left=NOSE_RIGHT; Id=7};
+        {Top=LAY_TOP; Right=BIG_TAIL; Bottom=NOSE_RIGHT; Left=SMALL_HEAD; Id=8};
+    ]
 
 let rotate input num = 
     match num with
     | 0 -> input
-    | 1 -> {Top=input.Left; Right=input.Top; Bottom=input.Right; Left=input.Bottom}
-    | 2 -> {Top=input.Bottom; Right=input.Left; Bottom=input.Top; Left=input.Right}
-    | 3 -> {Top=input.Right; Right=input.Bottom; Bottom=input.Left; Left=input.Top}
+    | 1 -> {Top=input.Left; Right=input.Top; Bottom=input.Right; Left=input.Bottom; Id=input.Id}
+    | 2 -> {Top=input.Bottom; Right=input.Left; Bottom=input.Top; Left=input.Right; Id=input.Id}
+    | 3 -> {Top=input.Right; Right=input.Bottom; Bottom=input.Left; Left=input.Top; Id=input.Id}
     | _ -> failwith "Invalid rotation"
 
 
@@ -46,9 +47,17 @@ let pop currentStack =
     | [] ->
         failwith "Stack is empty, can't pop"
 
+let stackContainsId stack id =
+    stack
+    |> List.exists (fun x -> x.Id = id)
+
 let printStack currentStack =
     currentStack
     |> List.map (fun x -> printf x + " ")
+
+let findEdgeOnLeft potentialTile edgeToFind = 
+    let potentialTiles = [potentialTile; rotate potentialTile 1; rotate potentialTile 2; rotate potentialTile 3;]
+    potentialTiles |> List.filter (fun x -> x.Left = edgeToFind)
 
 //is there a way to avoid this function "grouping"? can i unwind the recursion?
 let rec recurse (input:List<Tile>) =
@@ -59,7 +68,10 @@ let rec recurse (input:List<Tile>) =
     | _ -> ""
 and recurseRight (input:List<Tile>) = 
     let nextEdgeValue = input.[0].Right * -1
-    
+    let unusedTiles = bag |> List.filter (fun x -> not(stackContainsId input x.Id))
+    let unusedTilesWithEdgeMatchOnLeft = unusedTiles |> List.collect (fun x -> findEdgeOnLeft x nextEdgeValue)
+    for newTile in unusedTilesWithEdgeMatchOnLeft do
+        recurse (newTile::input) |> ignore
     //temp return string
     ""
 and recurseBottom (input:List<Tile>) =
